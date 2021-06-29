c9aa769934a69c5a6e3b5426591e837c1330bbe8
Siddharth <siddharth.muscat@gmail.com>
2021-06-29 11:41:30 +0530

    Temporarily comment out function check

--- src/parse-snips.js
+++ src/parse-snips.js
@@ -1,13 +1,11 @@
 export function parseDoubleChar(doubleChars, event, element) {
 	let doubleChar = doubleChars[event.key];
 
-	if (doubleChar instanceof Function) {
-		return doubleChar(element)
-	}
-	return doubleChar
+	// if (doubleChar instanceof Function) {
+	// 	return doubleChar(element)
+	// }
+	return doubleChar;
 
 }
 
-export function parseSnippet(snippets, key) {
-
-}
+export function parseSnippet(snippets, key) {}
