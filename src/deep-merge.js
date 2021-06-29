15b74db162a891b835235c7714a812fa229db44e
Siddharth <siddharth.muscat@gmail.com>
2021-06-29 09:39:05 +0530

    Create deep merge function

---
+++ src/deep-merge.js
@@ -0,0 +1,11 @@
+export function merge(target, source) {
+	for (const key of Object.keys(source)) {
+		if (source[key] instanceof Object) {
+			Object.assign(source[key], merge(target[key], source[key]));
+		}
+	}
+
+	// Join `target` and modified `source`
+	Object.assign(target || {}, source);
+	return target;
+}
