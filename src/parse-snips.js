16940e7682a23b4438fa8ad1cfcd1806b6b1e5df
Siddharth <siddharth.muscat@gmail.com>
2021-06-29 10:46:21 +0530

    Create snippet and double char parser

---
+++ src/parse-snips.js
@@ -0,0 +1,13 @@
+export function parseDoubleChar(doubleChars, event, element) {
+	let doubleChar = doubleChars[event.key];
+
+	if (doubleChar instanceof Function) {
+		return doubleChar(element)
+	}
+	return doubleChar
+
+}
+
+export function parseSnippet(snippets, key) {
+
+}
