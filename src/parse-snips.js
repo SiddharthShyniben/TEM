aa6f98aff95ad0acb287a17b24d225be41ca0b3c
Siddharth <siddharth.muscat@gmail.com>
2021-06-29 12:34:44 +0530

    Uncomment code commented during debugging

--- src/parse-snips.js
+++ src/parse-snips.js
@@ -1,9 +1,9 @@
 export function parseDoubleChar(doubleChars, event, element) {
 	let doubleChar = doubleChars[event.key];
 
-	// if (doubleChar instanceof Function) {
-	// 	return doubleChar(element)
-	// }
+	if (doubleChar instanceof Function) {
+		return doubleChar(element)
+	}
 	return doubleChar ?? {value: event.key, pos: 1};
 }
 
